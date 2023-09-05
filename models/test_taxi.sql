ca59ff026d570d9f422694deea5abf73db4f01b9
Ya5s3r <yasser.mushtaq@gmail.com>
2023-09-05 15:40:22 +0100

    Create dbt project

---
+++ models/test_taxi.sql
@@ -0,0 +1,26 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+with source_data as (
+
+    select *
+    from
+        `nyc-yellow-taxis-398112`.NYCYellowTaxis.RawTaxiData    
+    where
+        VendorID = 1
+    limit 10
+)
+
+select *
+from source_data
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
