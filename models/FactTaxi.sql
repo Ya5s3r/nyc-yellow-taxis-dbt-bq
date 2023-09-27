f4b259fc0236a01255da1e630a1b8ddb85f9b167
Ya5s3r <yasser.mushtaq@gmail.com>
2023-09-27 16:09:22 +0100

    added test fact table

---
+++ models/FactTaxi.sql
@@ -0,0 +1,12 @@
+select 
+    {{ dbt_utils.generate_surrogate_key(
+        [
+        'VendorID',
+        'tpep_pickup_datetime',
+        'tpep_dropoff_datetime'
+        ]
+    ) }} as taxi_id
+    ,*
+
+    from
+        `nyc-yellow-taxis-398112`.NYCYellowTaxis.RawTaxiData    
